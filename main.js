a44f15d217de0f6e99a7435adc114c02cef6a3fc
DiegxR <juandiegorojasarredono@gmail.com>
2023-01-23 00:21:30 -0500

    refactor project

--- main.js
+++ main.js
@@ -1,35 +1,51 @@
 import { getPokemons } from "./helpers/getPokemons.js"
 const pokemonInfo = document.getElementById('pokemon-info-container')
 const others = document.getElementById('others');
+
 const renderType = (data, type) =>{
     return data.filter((elem)=> elem.types[0].type.name.includes(type))
 } 
-const rend = async(name="") =>{
+
+const renderData = async(name="") =>{
     const promises = await getPokemons()
     const allData = await Promise.all(promises)
-    let filterPokemons = allData.filter(data => data.name.includes(name.toLocaleLowerCase()))
+    let filterPokemons = allData.filter(data => data.name.includes(name.toLowerCase()))
+    return {
+        filterPokemons,
+        allData
+    }
+}
+const rendArrays = async(name) =>{
+    const renderDat = await renderData(name);
+    const {filterPokemons, allData} = renderDat;
     let data = filterPokemons[0];
+    let types = []
     let typePokemons;
-    const types = []
-     allData.forEach(elem=>{
+    allData.forEach(elem=>{
         if(!types.includes(elem.types[0].type.name)){
             types.push(elem.types[0].type.name)
         }
      })
-     if(types.includes(name) ){
+     if(types.includes(name)){
         typePokemons = allData.filter(element => element.types[0].type.name == name.toLowerCase())
         data = typePokemons[0]
      }else{
          if(filterPokemons.length !== 0 ){
             typePokemons = renderType(allData, data.types[0].type.name)
         }else{
-            pokemonInfo.innerHTML += ` <p style ="font-weight:800; font-size:35px; color:red; ">NOT FOUND</p> `
+            pokemonInfo.innerHTML += `<p style ="font-weight:800; font-size:35px; color:red; ">NOT FOUND</p>`
         }
      }
-    console.log(types)
-   
-    console.log(typePokemons)
-                pokemonInfo.innerHTML = `
+     return {
+        typePokemons,
+        data,
+        types
+     }
+}
+const rendPokemon = async (name) =>{
+    const arrays = await rendArrays(name);
+    const {typePokemons, data} = arrays
+    pokemonInfo.innerHTML = `
                 <section id="pokemon-container">
                     <h1><img src='./img/${data.types[0].type.name}.png' alt="type" id="typeLogo">${data.name}</h1>
                     <figure>
@@ -63,66 +79,104 @@ const rend = async(name="") =>{
                     </div>
                 </section>
                 `
-                
+}
+  
+const rendOthers = async(name, more="") =>{
+const rendData  = await renderData(name)
+const {filterPokemons, allData} = rendData
+const arrays  = await rendArrays(name)
+const {typePokemons, data, types} = arrays
+
                 let count = 0
                 let checkedA = []
                 let checkedB = []
+                
                 others.innerHTML = ""
+                console.log(more)
             if(types.includes(name)){
                 typePokemons.forEach((elem, index) =>{
                     if((elem.id !== data.id) && count<4){
-                        others.innerHTML +=
-                            `<img src="${elem.sprites.front_default}" id="${elem.name}">`
-                            count ++
+                        if(more !== ""){
+                            if(index>more){
+                                others.innerHTML +=
+                                    `<img src="${elem.sprites.front_default}" id="${elem.name}" class="${data.types[0].type.name}">`
+                                    count ++
+                            }
+                        }else{
+                            others.innerHTML +=
+                                    `<img src="${elem.sprites.front_default}" id="${elem.name}" class="${data.types[0].type.name}">`
+                                    count ++
+                        }
+                        
                     }
                 })   
-            }else{
-                typePokemons.forEach((elem, index) =>{
-                    filterPokemons.forEach((element, ind)=>{
-                        if(elem.name == element.name){
-                            checkedA.push(index)
-                        }
-                        if(element.id !== data.id){
-                            checkedB.push(ind)
-                        }
+            }else{ 
+                    typePokemons.forEach((elem, index) =>{
+                        filterPokemons.forEach((element, ind)=>{
+                            if(elem.name == element.name){
+                                checkedA.push(index)
+                            }
+                            if(element.id !== data.id){
+                                checkedB.push(ind)
+                            }
+                        })
                     })
-                })
-            filterPokemons.forEach((element, ind)=>{
-                if(checkedB.includes(ind) && checkedB.length >= count && count<4){
-                    others.innerHTML +=
-                    `<img src="${element.sprites.front_default}" id="${element.name}">`
-                    count ++
-                }
-             })
-                typePokemons.forEach((elem, index) =>{
-                    if(!checkedA.includes(index) && count<4){
+                filterPokemons.forEach((element, ind)=>{
+                    if(checkedB.includes(ind) && checkedB.length >= count && count<4){
                         others.innerHTML +=
-                            `<img src="${elem.sprites.front_default}" id="${elem.name}">`
-                            count ++
+                        `<img src="${element.sprites.front_default}" id="${element.name}" class="${data.types[0].type.name}">`
+                        count ++
                     }
-                }) 
-            }
-                              
+                 })
+                    typePokemons.forEach((elem, index) =>{
+                        if(!checkedA.includes(index) && count<4){
+                            others.innerHTML +=
+                                `<img src="${elem.sprites.front_default}" id="${elem.name}" class="${data.types[0].type.name}">`
+                                count ++
+                        }
+                    }) 
+                }
 }
+                                                       
 const search = document.getElementById('search');
 const searchInput = document.getElementById('searchInput');
-
+const more = document.getElementById('more');
+let next = 0;
 search.addEventListener('submit', (e)=>{
-    
+    next = 0;
+    more.style.display = 'block'
+    console.log('subm')
     e.preventDefault()
     pokemonInfo.innerHTML = `
     <img src="./img/loadPokeball.png" id='pokeball'>
     `
     const searched = searchInput.value
-    rend(searched)
+    rendPokemon(searched)
+    rendOthers(searched)  
 })
 others.addEventListener('click', (e)=>{
     if(e.target.nodeName == "IMG"){
         pokemonInfo.innerHTML = `
     <img src="./img/loadPokeball.png" id='pokeball'>
     `
-        rend(e.target.id)
+    
+    rendPokemon(e.target.id)
+    rendOthers(e.target.id)  
+    console.log(e.target.id)
     }
+   
+})
+
+more.style.display = 'none'
+console.log(others.innerHTML)
+
+more.addEventListener('click', e =>{
+    if(next == 0){
+        next = 4
+    }
+    const type = e.composedPath()[2].children[1].children[3].classList.value   
+    rendOthers(type, next)
+    next += 4;
 })
      
     
